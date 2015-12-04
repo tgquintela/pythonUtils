52187394ee03782aa87f177bc1a38f566e67306b
antonio <tgq.spm@gmail.com>
2015-12-04 14:36:34 +0100

    Wheeler assignation in randomness module.

---
+++ pythonUtils/numpy_tools/randomness.py
@@ -0,0 +1,29 @@
+
+"""
+Randomness
+----------
+The module which groups all the utils which support random tasks.
+
+"""
+
+import numpy as np
+
+
+def wheeler_assignation(prop, r):
+    """Assign the vote to the box who contains r.
+
+    Parameters
+    ----------
+    prop: numpy.ndarray
+        the vector of normalized probabilities for each option.
+    r: float [0, 1)
+        a random number between 0 and 1.
+
+    """
+
+    prop2 = np.cumsum(prop)
+    for i in range(prop.shape[0]):
+        if prop2[i] >= r:
+            break
+    return i
+
