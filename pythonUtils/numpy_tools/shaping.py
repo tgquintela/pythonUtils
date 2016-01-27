bc0cdc6572b0517925aee3840220cca7efde562d
antonio <tgq.spm@gmail.com>
2016-01-27 17:46:15 +0100

    General tools for ensuring shape in a array.

--- pythonUtils/numpy_tools/shaping.py
+++ pythonUtils/numpy_tools/shaping.py
@@ -17,3 +17,34 @@ def shaping_dim(array, ndim):
     newshape = tuple(list(array.shape) + [1 for i in range(dim_extend)])
     array = array.reshape(newshape)
     return array
+
+
+def ensure_2dim(array, axis=0, sh_known=(None, None)):
+    "TODO: move to python utils"
+    ## Ensure 2 dimensions in the array
+    sh = array.shape
+    if len(sh) == 1:
+        new_sh = (sh[0], 1) if axis == 0 else (1, sh[0])
+    elif len(sh) == 2:
+        new_sh = sh
+    ## Ensure known shape
+    notnone_ids = [i for i in range(len(sh_known)) if sh_known[i] is not None]
+    if len(notnone_ids) == 0:
+        pass
+    if len(notnone_ids) == 1:
+        if new_sh[notnone_ids] == sh_known[notnone_ids]:
+            pass
+        elif new_sh[::-1][notnone_ids] == sh_known[notnone_ids]:
+            new_sh = new_sh[::-1]
+        else:
+            raise Exception("Impossible to fit the conditions.")
+    elif len(notnone_ids) == 2:
+        if new_sh == sh_known:
+            pass
+        elif new_sh[::-1] == sh_known:
+            new_sh = new_sh[::-1]
+        else:
+            raise Exception("Impossible to fit the conditions.")
+    ## Transform array
+    array = array.reshape(new_sh)
+    return array
