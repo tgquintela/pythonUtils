1d3cfc0de6a16516e855fa3f3c49e08fcf0633ce
antonio <tgq.spm@gmail.com>
2016-01-27 17:45:32 +0100

    New formatting string tools.

--- pythonUtils/CodingText/__init__.py
+++ pythonUtils/CodingText/__init__.py
@@ -27,3 +27,22 @@ def encode_dictlist(dictlist):
             key_d = e
         new_dictlist[key_d] = encode_list(dictlist[e])
     return new_dictlist
+
+
+import unicodedata
+def strip_accents(s):
+   try:
+       s = unicode(s)
+   except:
+       s = s.decode("utf-8")
+   return ''.join(c for c in unicodedata.normalize('NFD', s)
+                  if unicodedata.category(c) != 'Mn')
+
+def mapper_unique_str(s)
+   "Mapping function of a string to a number using hash."
+   formatter = lambda x: " ".join([strip_accents(e.strip().lower()) for e in x.split(" ")])
+   return hash(formatter(s))
+
+
+
+
