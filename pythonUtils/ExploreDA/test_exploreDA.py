9d4132684ed4c446ec089d5ccf5a376ba05644ea
antonio <tgq.spm@gmail.com>
2016-12-04 16:23:59 +0100

    Studying test ExploreDA.

--- pythonUtils/ExploreDA/test_exploreDA.py
+++ pythonUtils/ExploreDA/test_exploreDA.py
@@ -49,9 +49,9 @@ def test():
     ranges_compute(contdata, 5)
     cont_count(df, 'a', 5)
     log_cont_count(df, 'a', 5)
-#
-#    # Coordinate variables
-#    mean_coord_by_values(df, ['a', 'b'], 'c')
+
+    # Coordinate variables
+    mean_coord_by_values(df, ['a', 'b'], 'c')
 
 #    # Temporal variable
 #    date_ranges = np.linspace(timedata.min(), timedata.max(), 5)[1:-1]
